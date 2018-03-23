6a9cb0d892ae98e4b8bfc1532a1f53a907f5c7c4
Will Rowe <will.rowe@stfc.ac.uk>
2018-03-23 12:54:50 +0000

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ copyright = '2018, Will Rowe'
 author = 'Will Rowe'
 
 # The short X.Y version
-version = '0.2'
+version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.4'
 
 
 # -- General configuration ---------------------------------------------------
