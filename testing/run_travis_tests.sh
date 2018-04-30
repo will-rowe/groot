d47995b4582750b866126efa384d8142388273eb
Will Rowe <will.rowe@stfc.ac.uk>
2018-04-30 13:02:50 +0100

    fixing test

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -4,10 +4,10 @@
 go build
 
 # download the database
-./groot get -d groot-core-db
+./groot get -d groot-core-db -o ./db
 
 # index the example
-./groot index -p 1 -i ./groot-core-db.90 -o test-index
+./groot index -p 1 -i ./db/groot-core-db.90 -o test-index
 
 # align the test reads
 ./groot align -p 1 -i test-index -f testing/full-argannot-perfect-reads-small.fq.gz > out.bam
